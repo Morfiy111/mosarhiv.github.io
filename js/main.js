55b362e2b1f6a3bb5ea9ceced430b4116d353f40
morfiy <keks46@yandex.ru>
2022-08-01 09:01:15 +0300

    first

--- js/main.js
+++ js/main.js
@@ -16,3 +16,22 @@ let timelineSwiper = new Swiper('.timeline .swiper-container', {
         }
     }
 });
+
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items: 1,
+    loop: true,
+    margin: 0,
+    nav: true,
+    dots: true,
+    autoplay: true,
+    autoplayTimeout: 4000,
+    smartSpeed: 700,
+    autoplayHoverPause: true
+});
+$('.play').on('click', function () {
+    owl.trigger('autoplay.play.owl', [4000])
+})
+$('.stop').on('click', function () {
+    owl.trigger('autoplay.stop.owl')
+})
